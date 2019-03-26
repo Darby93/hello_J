26b5c842918aba5ab95c78b7d1252cc2a2075631
DarbyJ <jony8674@gmail.com>
2019-03-26 20:55:56 +0900

    - node 공부 0326

---
+++ javascript/nodejs/NodeExample1/ch03_test2.js
@@ -0,0 +1,12 @@
+var person = {};
+
+person['name'] = '소녀시대';
+person['age'] = 20;
+person.mobile = '010-1000-1000';
+
+console.log('이름 : ' + person.name);
+console.log('나이 : ' + person['age']);
+
+console.log(person);
+
+
---
+++ javascript/nodejs/NodeExample1/ch03_test4.js
@@ -0,0 +1,16 @@
+var person = {};
+
+person.name = '소녀시대';
+person['age'] = 20;
+person.add = function(a,b){
+    return a+b;
+};
+
+var oper = function(a,b){
+    return 10*12;
+};
+
+
+
+
+console.log(person.add(20,30));
