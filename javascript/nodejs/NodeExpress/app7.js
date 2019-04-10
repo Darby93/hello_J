0bc07249b94ff15d2418e46c842bf4ab4630086f
DarbyJ <jony8674@gmail.com>
2019-04-10 14:10:48 +0900

    Update file

---
+++ javascript/nodejs/NodeExpress/app7.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var http = require('http');
+var static = require('serve-static');
+var path = require('path');
+
+var bodyParser = require('body-parser');
+
+
+var app = express();
+
+app.set('port', process.env.PORT||3000);
+// app.use(static(path.join(__dirname, 'public')));
+app.use('/public', static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+    console.log('첫 번째 미들웨어 실행');
+
+    var userAgent = req.header('User-Agent');
+    var paramName = req.body.name || req.query.name;
+
+    res.send('<h3>서버에서 응답 User-Agent -> ' + userAgent  +'</h3>'
+    + '<h3>Param Name -> '+ paramName + '</h3>');
+    
+});
+
+var server = http.createServer(app).listen(app.get('port'),function(){
+    console.log('익스프레스로 웹서버 실행 : ' + app.get('port'));
+    
+});
